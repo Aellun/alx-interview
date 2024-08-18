8192d7bdd86cc65f54ee3b363bf6fb7338020847
aellun <okello.k.otieno@gmail.com>
2024-08-18 17:39:28 +0300

    updatedcode

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,75 +1,65 @@
 #!/usr/bin/python3
-'''Script that reads stdin line by line and computes metrics.
+'''script that reads stdin line by line and computes metrics
     After every 10 lines and/or a keyboard interruption (CTRL + C),
     print these statistics from the beginning:
         Total file size: File size: <total size>
         where <total size> is the sum of all previous <file size>
         Number of lines by status code:
-        possible status code: 200, 301, 400, 401, 403, 404, 405, and 500
-        If a status code doesn’t appear or is not an integer,
-        don’t print anything for this status code.
-        Format: <status code>: <number>
-        Status codes should be printed in ascending order.
+        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+        if a status code doesn’t appear or is not an integer,
+            don’t print anything for this status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
 '''
 import sys
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print status
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-# Dictionary to keep track of counts for each status code
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_file_size = 0
+code = 0
 counter = 0
-
-def print_stats():
-    """
-    Print the current statistics:
-    - Total file size
-    - Number of occurrences for each status code (if greater than zero)
-    """
-    print('File size: {}'.format(total_file_size))
-    for key, value in sorted(dict_sc.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+# Dictionary to keep track of counts for each status code
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     # Read lines from standard input
     for line in sys.stdin:
-        # Split the line into components based on spaces
-        line_list = line.split()
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-        # Process lines with at least 5 components
-        if len(line_list) > 4:
-            try:
-                status_code = line_list[-2]
-                file_size = int(line_list[-1])
+        if len(parsed_line) > 2:
+            counter += 1
 
-                if status_code in dict_sc:
-                    dict_sc[status_code] += 1
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-                total_file_size += file_size
-                counter += 1
-
-                # Print statistics after every 10 lines
-                if counter == 10:
-                    print_stats()
-                    counter = 0
-
-            except ValueError:
-                # Handle cases where file size is not an integer
-                continue
-
-except KeyboardInterrupt:
-    # Print statistics when interrupted by the user (Ctrl + C)
-    print_stats()
-    sys.exit(0)  # Exit the script gracefully
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+            # Print statistics after every 10 lines
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 finally:
-    # Print final statistics when processing is complete
-    print_stats()
+    print_msg(dict_sc, total_file_size)
